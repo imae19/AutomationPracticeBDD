2e2111ff630eb1e73ad8271a2337ecdda9a97ceb
imae19 <ms.chaijan@gmail.com>
2022-10-15 13:11:19 -0400

    add

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "step_definitions",
         stepNotifications = true,
-        dryRun = false)
+        dryRun = false
+)
 
 public class CukesRunner {
 
